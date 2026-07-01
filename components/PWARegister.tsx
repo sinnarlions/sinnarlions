5894518007882781110182c7455eb3bb83075f1c
Jitendra Jagtap <sinnarlions@gmail.com>
2026-07-01 16:16:52 +0530

    Temporarily disable service worker

--- components/PWARegister.tsx
+++ components/PWARegister.tsx
@@ -1,21 +1,5 @@
 "use client";
 
-import { useEffect } from "react";
-
 export default function PWARegister() {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", async () => {
-        try {
-          const registration = await navigator.serviceWorker.register("/sw.js");
-
-          console.log("✅ Service Worker registered:", registration.scope);
-        } catch (error) {
-          console.error("❌ Service Worker registration failed:", error);
-        }
-      });
-    }
-  }, []);
-
   return null;
 }
